f17288a0f79d47443778ddc3113e41d538e03065
Peasplayer <63916422+Peasplayer@users.noreply.github.com>
2021-07-04 15:37:27 +0200

    Another Bug in Watermark.cs

--- PeasAPI/Watermark.cs
+++ PeasAPI/Watermark.cs
@@ -20,8 +20,6 @@ namespace PeasAPI
                 __instance.text.text += $"\n<color=#ff0000ff>PeasAPI {PeasApi.Version} <color=#ffffffff> by <color=#ff0000ff>Peasplayer\n<color=#ffffffff>Reactor-Framework";
                 __instance.transform.position -= new Vector3(0, 0.5f, 0);
                 
-                AccountManager.Instance.accountTab.gameObject.SetActive(false);
-                
                 foreach (var _object in GameObject.FindObjectsOfTypeAll(Il2CppType.Of<GameObject>()))
                     if (_object.name.Contains("ReactorVersion"))
                         GameObject.Destroy(_object);
