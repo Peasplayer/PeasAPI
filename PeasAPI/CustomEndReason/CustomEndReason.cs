2b44755dd9a49be875a0719308728bd69c32dd76
Peasplayer <peasplayer@peasplayer.tk>
2021-07-04 21:14:01 +0200

    Fixed CustomEndReason if you don't have a role

--- PeasAPI/CustomEndReason/CustomEndReason.cs
+++ PeasAPI/CustomEndReason/CustomEndReason.cs
@@ -32,7 +32,7 @@ namespace PeasAPI.CustomEndReason
             {
                 winners.Add(winner.GetPlayerInfo());
             }
-            
+
             EndReasonManager.Color = color;
             EndReasonManager.Winners = winners;
             EndReasonManager.Stinger = stinger;
@@ -46,80 +46,135 @@ namespace PeasAPI.CustomEndReason
             {
                 var role = player.GetRole();
 
-
                 MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId,
                     (byte) CustomRpc.CustomEndReason, SendOption.None, -1);
 
-                writer.Write(role.Color.r);
-                writer.Write(role.Color.g);
-                writer.Write(role.Color.b);
-
-                var _winners = new List<byte>();
-
-                if (role.Team == Team.Crewmate)
+                if (role == null)
                 {
-                    writer.Write("crew");
-
-                    _winners.Add(player.PlayerId);
-                    foreach (var _player in GameData.Instance.AllPlayers)
+                    if (player.Data.IsImpostor)
                     {
-                        if (_player.PlayerId != player.PlayerId && !_player.IsImpostor)
-                            _winners.Add(_player.PlayerId);
+                        writer.Write(Palette.ImpostorRed.r);
+                        writer.Write(Palette.ImpostorRed.g);
+                        writer.Write(Palette.ImpostorRed.b);
+
+                        var _winners = new List<byte>();
+                        _winners.Add(player.PlayerId);
+                        foreach (var _player in GameData.Instance.AllPlayers)
+                        {
+                            if (_player.PlayerId != player.PlayerId && _player.IsImpostor)
+                                _winners.Add(_player.PlayerId);
+                        }
+                        
+                        var winners = new List<GameData.PlayerInfo>();
+                        foreach (var winner in _winners)
+                        {
+                            winners.Add(winner.GetPlayerInfo());
+                        }
+
+                        EndReasonManager.Color = Palette.ImpostorRed;
+                        EndReasonManager.Winners = winners;
+                        EndReasonManager.Stinger = "impostor";
+                    }
+                    else
+                    {
+                        writer.Write(Palette.CrewmateBlue.r);
+                        writer.Write(Palette.CrewmateBlue.g);
+                        writer.Write(Palette.CrewmateBlue.b);
+                        
+                        var _winners = new List<byte>();
+                        _winners.Add(player.PlayerId);
+                        foreach (var _player in GameData.Instance.AllPlayers)
+                        {
+                            if (_player.PlayerId != player.PlayerId && !_player.IsImpostor)
+                                _winners.Add(_player.PlayerId);
+                        }
+                        
+                        var winners = new List<GameData.PlayerInfo>();
+                        foreach (var winner in _winners)
+                        {
+                            winners.Add(winner.GetPlayerInfo());
+                        }
+
+                        EndReasonManager.Color = Palette.CrewmateBlue;
+                        EndReasonManager.Winners = winners;
+                        EndReasonManager.Stinger = "crew";
+                    }
+                }
+                else
+                {
+                    writer.Write(role.Color.r);
+                    writer.Write(role.Color.g);
+                    writer.Write(role.Color.b);
+
+                    var _winners = new List<byte>();
+
+                    if (role.Team == Team.Crewmate)
+                    {
+                        writer.Write("crew");
+
+                        _winners.Add(player.PlayerId);
+                        foreach (var _player in GameData.Instance.AllPlayers)
+                        {
+                            if (_player.PlayerId != player.PlayerId && !_player.IsImpostor)
+                                _winners.Add(_player.PlayerId);
+                        }
+
+                        writer.Write(_winners.Count);
+                        writer.Write(_winners.ToArray());
+                    }
+                    else if (role.Team == Team.Impostor)
+                    {
+                        writer.Write("impostor");
+
+                        _winners.Add(player.PlayerId);
+                        foreach (var _player in GameData.Instance.AllPlayers)
+                        {
+                            if (_player.PlayerId != player.PlayerId && _player.IsImpostor)
+                                _winners.Add(_player.PlayerId);
+                        }
+
+                        writer.Write(_winners.Count);
+                        writer.Write(_winners.ToArray());
+                    }
+                    else if (role.Team == Team.Alone)
+                    {
+                        writer.Write("impostor");
+
+                        _winners.Add(player.PlayerId);
+                        writer.Write(_winners.Count);
+                        writer.Write(_winners.ToArray());
+                    }
+                    else if (role.Team == Team.Role)
+                    {
+                        writer.Write("impostor");
+
+                        _winners.Add(player.PlayerId);
+                        foreach (var _player in GameData.Instance.AllPlayers)
+                        {
+                            if (_player.PlayerId != player.PlayerId && _player.GetRole() == player.GetRole())
+                                _winners.Add(_player.PlayerId);
+                        }
+
+                        writer.Write(_winners.Count);
+                        writer.Write(_winners.ToArray());
                     }
                     
-                    writer.Write(_winners.Count);
-                    writer.Write(_winners.ToArray());
-                }
-                else if (role.Team == Team.Impostor)
-                {
-                    writer.Write("impostor");
-
-                    _winners.Add(player.PlayerId);
-                    foreach (var _player in GameData.Instance.AllPlayers)
+                    var winners = new List<GameData.PlayerInfo>();
+                    foreach (var winner in _winners)
                     {
-                        if (_player.PlayerId != player.PlayerId && _player.IsImpostor)
-                            _winners.Add(_player.PlayerId);
+                        winners.Add(winner.GetPlayerInfo());
                     }
-
-                    writer.Write(_winners.Count);
-                    writer.Write(_winners.ToArray());
-                }
-                else if (role.Team == Team.Alone)
-                {
-                    writer.Write("impostor");
-
-                    _winners.Add(player.PlayerId);
-                    writer.Write(_winners.Count);
-                    writer.Write(_winners.ToArray());
-                } else if (role.Team == Team.Role)
-                {
-                    writer.Write("impostor");
-                    
-                    _winners.Add(player.PlayerId);
-                    foreach (var _player in GameData.Instance.AllPlayers)
-                    {
-                        if (_player.PlayerId != player.PlayerId && _player.GetRole() == player.GetRole())
-                            _winners.Add(_player.PlayerId);
-                    }
-                    writer.Write(_winners.Count);
-                    writer.Write(_winners.ToArray());
-                }
-
-                AmongUsClient.Instance.FinishRpcImmediately(writer);
-
-                var winners = new List<GameData.PlayerInfo>();
-                foreach (var winner in _winners)
-                {
-                    winners.Add(winner.GetPlayerInfo());
+                
+                    EndReasonManager.Color = role.Color;
+                    EndReasonManager.Winners = winners;
+                    if (role.Team == Team.Crewmate)
+                        EndReasonManager.Stinger = "crew";
+                    else
+                        EndReasonManager.Stinger = "impostor";
                 }
                 
-                EndReasonManager.Color = role.Color;
-                EndReasonManager.Winners = winners;
-                if (role.Team == Team.Crewmate)
-                    EndReasonManager.Stinger = "crew";
-                else
-                    EndReasonManager.Stinger = "impostor";
-
+                AmongUsClient.Instance.FinishRpcImmediately(writer);
+                
                 ShipStatus.RpcEndGame(EndReasonManager.CustomGameOverReason, false);
             }
         }
