6864279fcd3fecb9c7d302638a91bc1a008b67f0
Peasplayer <peasplayer@peasplayer.tk>
2021-12-04 21:26:15 +0100

    Added custom visors

--- PeasAPI/Data.cs
+++ PeasAPI/Data.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Reactor.Extensions;
 using UnityEngine;
 
 namespace PeasAPI
@@ -24,6 +28,126 @@ namespace PeasAPI
                 Role = role.IsNullOrWhiteSpace() ? team : role;
                 RoleDescription = roleDescription.IsNullOrWhiteSpace() ? teamDescription : roleDescription;
                 RoleColor = roleColor ?? teamColor;
+            }
+        }
+        
+        public readonly struct Hat
+        {
+            public readonly string Name;
+            public readonly string ImagePath;
+            public readonly Assembly Assembly;
+            public readonly bool InFront;
+            public readonly bool NoBounce;
+            public readonly Vector2 ChipOffset;
+            public readonly Sprite BackImage;
+            public readonly Sprite FloorImage;
+            
+            public Hat(string name, string imagePath, Assembly assembly, bool inFront, bool noBounce, Vector2 chipOffset, Sprite backImage, Sprite floorImage)
+            {
+                Name = name;
+                ImagePath = imagePath;
+                Assembly = assembly;
+                InFront = inFront;
+                NoBounce = noBounce;
+                ChipOffset = chipOffset;
+                BackImage = backImage;
+                FloorImage = floorImage;
+            }
+            
+            public HatBehaviour CreateHat()
+            {
+                try
+                {
+                    Texture2D tex = new Texture2D(128, 128, TextureFormat.ARGB32, false);
+                    Stream myStream = Assembly.GetManifestResourceStream(ImagePath);
+                    byte[] data = myStream.ReadFully();
+                    ImageConversion.LoadImage(tex, data, false);
+
+                    var newHat = ScriptableObject.CreateInstance<HatBehaviour>();
+                    newHat.MainImage = newHat.LeftMainImage = Sprite.Create(
+                        tex,
+                        new Rect(0, 0, tex.width, tex.height),
+                        new Vector2(0.53f, 0.575f),
+                        tex.width * 0.375f
+                    );
+                    
+                    newHat.ProductId = $"+{Name}";
+                    newHat.Order += 100;
+                    newHat.Free = true;
+                    newHat.StoreName = Name;
+                    newHat.name = Name;
+                    
+                    newHat.InFront = InFront;
+                    newHat.NoBounce = NoBounce;
+                    newHat.ChipOffset = ChipOffset;
+                    newHat.BackImage = newHat.LeftBackImage = BackImage;
+                    newHat.ClimbImage = newHat.LeftClimbImage = BackImage;
+                    newHat.FloorImage = newHat.LeftFloorImage = FloorImage;
+                    
+                    return newHat;
+                }
+                catch (Exception e)
+                {
+                    PeasAPI.Logger.LogError($"Error while creating a hat: {e.StackTrace}");
+                }
+
+                return null;
+            }
+        }
+        
+        public readonly struct Visor
+        {
+            public readonly string Name;
+            public readonly string ImagePath;
+            public readonly Assembly Assembly;
+            public readonly Vector2 ChipOffset;
+            public readonly Sprite ClimbImage;
+            public readonly Sprite FloorImage;
+            
+            public Visor(string name, string imagePath, Assembly assembly, Vector2 chipOffset, Sprite climbImage, Sprite floorImage)
+            {
+                Name = name;
+                ImagePath = imagePath;
+                Assembly = assembly;
+                ChipOffset = chipOffset;
+                ClimbImage = climbImage;
+                FloorImage = floorImage;
+            }
+            
+            public VisorData CreateVisor()
+            {
+                try
+                {
+                    Texture2D tex = new Texture2D(128, 128, TextureFormat.ARGB32, false);
+                    Stream myStream = Assembly.GetManifestResourceStream(ImagePath);
+                    byte[] data = myStream.ReadFully();
+                    ImageConversion.LoadImage(tex, data, false);
+
+                    var newVisor = ScriptableObject.CreateInstance<VisorData>();
+                    newVisor.IdleFrame = newVisor.LeftIdleFrame = Sprite.Create(
+                        tex,
+                        new Rect(0, 0, tex.width, tex.height),
+                        new Vector2(0.53f, 0.575f),
+                        tex.width * 0.375f
+                    );
+                    
+                    newVisor.ProductId = $"+{Name}";
+                    newVisor.Order += 100;
+                    newVisor.Free = true;
+                    newVisor.name = Name;
+                    
+                    newVisor.ChipOffset = ChipOffset;
+                    newVisor.ClimbFrame = ClimbImage;
+                    newVisor.FloorFrame = FloorImage;
+                    
+                    return newVisor;
+                }
+                catch (Exception e)
+                {
+                    PeasAPI.Logger.LogError($"Error while creating a visor: {e.StackTrace}");
+                }
+
+                return null;
             }
         }
     }
--- PeasAPI/Managers/CustomHatManager.cs
+++ PeasAPI/Managers/CustomHatManager.cs
@@ -1,81 +1,16 @@
-﻿using System;
-using HarmonyLib;
+﻿using HarmonyLib;
 using System.Collections.Generic;
-using System.IO;
 using System.Reflection;
 using UnityEngine;
-using Reactor.Extensions;
+using static PeasAPI.Data;
 
 namespace PeasAPI.Managers
 {
     public class CustomHatManager
     {
         public static List<Hat> CustomHats = new List<Hat>();
+        public static List<Visor> CustomVisors = new List<Visor>();
 
-        public readonly struct Hat
-        {
-            public readonly string Name;
-            public readonly string ImagePath;
-            public readonly Assembly Assembly;
-            public readonly bool InFront;
-            public readonly bool NoBounce;
-            public readonly Vector2 ChipOffset;
-            public readonly Sprite BackImage;
-            public readonly Sprite FloorImage;
-            
-            public Hat(string name, string imagePath, Assembly assembly, bool inFront, bool noBounce, Vector2 chipOffset, Sprite backImage, Sprite floorImage)
-            {
-                Name = name;
-                ImagePath = imagePath;
-                Assembly = assembly;
-                InFront = inFront;
-                NoBounce = noBounce;
-                ChipOffset = chipOffset;
-                BackImage = backImage;
-                FloorImage = floorImage;
-            }
-            
-            public HatBehaviour CreateHat()
-            {
-                try
-                {
-                    Texture2D tex = new Texture2D(128, 128, TextureFormat.ARGB32, false);
-                    Stream myStream = Assembly.GetManifestResourceStream(ImagePath);
-                    byte[] data = myStream.ReadFully();
-                    ImageConversion.LoadImage(tex, data, false);
-
-                    var newHat = ScriptableObject.CreateInstance<HatBehaviour>();
-                    newHat.MainImage = newHat.LeftMainImage = Sprite.Create(
-                        tex,
-                        new Rect(0, 0, tex.width, tex.height),
-                        new Vector2(0.53f, 0.575f),
-                        tex.width * 0.375f
-                    );
-                    
-                    newHat.ProductId = $"+{Name}";
-                    newHat.Order += 100;
-                    newHat.Free = true;
-                    newHat.StoreName = Name;
-                    newHat.name = Name;
-                    
-                    newHat.InFront = InFront;
-                    newHat.NoBounce = NoBounce;
-                    newHat.ChipOffset = ChipOffset;
-                    newHat.BackImage = newHat.LeftBackImage = BackImage;
-                    newHat.ClimbImage = newHat.LeftClimbImage = BackImage;
-                    newHat.FloorImage = newHat.LeftFloorImage = FloorImage;
-                    
-                    return newHat;
-                }
-                catch (Exception e)
-                {
-                    PeasAPI.Logger.LogError($"Error while creating a hat: {e.StackTrace}");
-                }
-
-                return null;
-            }
-        }
-        
         public static void RegisterNewHat(string name, string imagePath, Vector2 chipOffset = new Vector2(), bool inFront = true, bool noBounce = true, Sprite backImage = null, Sprite floorImage = null)
         {
             var hat = new Hat(name, imagePath, Assembly.GetCallingAssembly(), inFront, noBounce, chipOffset, backImage, floorImage);
@@ -84,6 +19,16 @@ namespace PeasAPI.Managers
             
             if (PeasAPI.Logging)
                 PeasAPI.Logger.LogInfo($"Registered hat {name} from {Assembly.GetCallingAssembly().GetName().Name}");
+        }
+        
+        public static void RegisterNewVisor(string name, string imagePath, Vector2 chipOffset = new Vector2(), Sprite climbImage = null, Sprite floorImage = null)
+        {
+            var visor = new Visor(name, imagePath, Assembly.GetCallingAssembly(), chipOffset, climbImage, floorImage);
+            
+            CustomVisors.Add(visor);
+            
+            if (PeasAPI.Logging)
+                PeasAPI.Logger.LogInfo($"Registered visor {name} from {Assembly.GetCallingAssembly().GetName().Name}");
         }
     }
 
@@ -101,8 +46,12 @@ namespace PeasAPI.Managers
 
             foreach (var hat in CustomHatManager.CustomHats)
                 __instance.AllHats.Add(hat.CreateHat());
+            foreach (var visor in CustomHatManager.CustomVisors)
+                __instance.AllVisors.Add(visor.CreateVisor());
             
             __instance.AllHats.Sort((Il2CppSystem.Comparison<HatBehaviour>)((h1, h2) => h2.ProductId.CompareTo(h1.ProductId)));
+            
+            __instance.AllVisors.Sort((Il2CppSystem.Comparison<VisorData>)((h1, h2) => h2.ProductId.CompareTo(h1.ProductId)));
         }
     }
 }
