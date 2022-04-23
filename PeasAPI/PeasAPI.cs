8ff7670139843ab1e2c5378eebca19a661f3365f
Peasplayer <peasplayer@peasplayer.tk>
2022-04-23 19:33:02 +0200

    1.8.3

--- PeasAPI/PeasAPI.cs
+++ PeasAPI/PeasAPI.cs
@@ -21,7 +21,7 @@ namespace PeasAPI
     public class PeasAPI : BasePlugin
     {
         public const string Id = "tk.peasplayer.amongus.api";
-        public const string Version = "1.8.2";
+        public const string Version = "1.8.3";
 
         public Harmony Harmony { get; } = new Harmony(Id);
 
